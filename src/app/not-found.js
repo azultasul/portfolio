aa7af12d7ba8660487ced64ef2b9e4ffd446910b
azultasul <azultasul@gmail.com>
2024-09-11 23:35:02 +0900

    404 소소한 수정

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,17 +1,18 @@
+import Link from 'next/link'
 import Countdown from '@/components/Countdown'
 
 const NotFound = () => {
   return (
     <div className="util">
       <div className="util__text">
-        <h1>404 : 없는 페이지입니다 🙅🏻‍♂️</h1>
+        <h1>404 : 없는 페이지🙅🏻‍♂️</h1>
 
         <div className="tag-wrap">
-          <button className="btn btn--line">
+          <Link href="/" scroll={false} className="btn btn--line">
             <h3>
-              🏠<span>홈으로 돌아가기</span> <Countdown path="/" time={4} />
+              🏠<span> 홈으로 가기</span> <Countdown path="/" time={4} />
             </h3>
-          </button>
+          </Link>
         </div>
       </div>
     </div>
--- src/app/project/not-found.js
+++ src/app/project/not-found.js
@@ -1,17 +1,18 @@
+import Link from 'next/link'
 import Countdown from '@/components/Countdown'
 
 const NotFound = () => {
   return (
     <div className="util">
       <div className="util__text">
-        <h1>404 : 없는 페이지입니다 🙅🏻‍♂️</h1>
+        <h1>404 : 없는 페이지🙅🏻‍♂️</h1>
 
         <div className="tag-wrap">
-          <button className="btn btn--line">
+          <Link href="/project" scroll={false} className="btn btn--line">
             <h3>
-              🗃️<span>프로젝트 보기</span> <Countdown path="/project" time={4} />
+              🗃️<span> 프로젝트 보기</span> <Countdown path="/project" time={4} />
             </h3>
-          </button>
+          </Link>
         </div>
       </div>
     </div>
